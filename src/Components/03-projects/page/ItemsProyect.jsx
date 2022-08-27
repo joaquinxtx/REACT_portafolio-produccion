f8ab9f862b5472a510c6980efb4405cd95f682fe
joaquinxtx <thejotex1998@gmail.com>
2022-08-26 23:33:03 -0300

    transition

--- src/Components/03-projects/page/ItemsProyect.jsx
+++ src/Components/03-projects/page/ItemsProyect.jsx
@@ -1,39 +1,58 @@
 import React from "react";
-import 'react-bootstrap'
+import "react-bootstrap";
 import "./ItemProyect.css";
 
-export const ItemsProyect = ({img,alt,nameProyect,deQueTrata,description,tech2,tech3,tech4,tech5,tech6,tech}) => {
+export const ItemsProyect = ({
+  img,
+  alt,
+  nameProyect,
+  deQueTrata,
+  description,
+  tech2,
+  tech3,
+  tech4,
+  tech5,
+  tech6,
+  tech,
+}) => {
   return (
-    <div className="gridItemProyect "  data-aos="fade-up"data-aos-delay="100"
-    data-aos-duration="700">
-        <div className="divImgProycto"data-aos="flip-right"data-aos-delay="100"
-            data-aos-duration="700">
-            <img src={img} alt={alt} className="imgProyectItem" />
-
+    <>
+    
+      <div
+        className="gridItemProyect hola"
+        data-aos="fade-up"
+        data-aos-delay="100"
+        data-aos-duration="700"
+      >
+        <div
+          className="divImgProycto"
+          data-aos="flip-right"
+          data-aos-delay="100"
+          data-aos-duration="700"
+        >
+          <img src={img} alt={alt} className="imgProyectItem" />
         </div>
-      
-      <div className="divGridItem">
-        <h2 >
-          {nameProyect}
-          <span >{deQueTrata} </span>
-        </h2>
 
-        <p >
-          {description}
-        </p>
+        <div className="divGridItem">
+          <h2>
+            {nameProyect}
+            <span>{deQueTrata} </span>
+          </h2>
 
-        <h3 >Tecnolog&iacute;as utilizadas:</h3>
+          <p>{description}</p>
 
-        <ul className=" tech-used">
-          <li>{tech} </li>
-          <li>{tech2} </li>
-          <li>{tech3} </li>
-          <li>{tech4} </li>
-          <li>{tech5} </li>
-          <li>{tech6} </li>
-          
-        </ul>
+          <h3>Tecnolog&iacute;as utilizadas:</h3>
+
+          <ul className=" tech-used">
+            <li>{tech} </li>
+            <li>{tech2} </li>
+            <li>{tech3} </li>
+            <li>{tech4} </li>
+            <li>{tech5} </li>
+            <li>{tech6} </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
